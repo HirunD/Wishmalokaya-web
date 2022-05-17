6c3610e30c6372b340f1a4c1b2c94fb35874fba8
HirunD <hirundwickramasinghe@gmail.com>
2022-05-17 21:53:46 +0530

    messages page init commit

--- js/dp.js
+++ js/dp.js
@@ -1,18 +1,22 @@
-// {/* <button id="upload_widget" class="cloudinary-button">Upload files</button> */}
+// // {/* <button id="upload_widget" class="cloudinary-button">Upload files</button> */}
 
-// {/* <script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript"></script>   */}
+// // {/* <script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript"></script>   */}
 
-// {/* <script type="text/javascript">   */}
-var myWidget = cloudinary.createUploadWidget({
-  cloudName: 'wishmalokaya', 
-  uploadPreset: 'wishmalokaya'}, (error, result) => { 
-    if (!error && result && result.event === "success") { 
-      console.log('Done! Here is the image info: ', result.info); 
-    }
-  }
-)
+// // {/* <script type="text/javascript">   */}
+// var myWidget = cloudinary.createUploadWidget({
+//   cloudName: 'wishmalokaya', 
+//   uploadPreset: 'wishmalokaya'}, (error, result) => { 
+//     if (!error && result && result.event === "success") { 
+//       console.log('Done! Here is the image info: ', result.info); 
+//     }
+//   }
+// )
 
-document.getElementById("upload_widget").addEventListener("click", function(){
-    myWidget.open();
-  }, false);
-// </script>
+// document.getElementById("upload_widget").addEventListener("click", function(){
+//     myWidget.open();
+//   }, false);
+// // </script>
+
+// var date = new getDate();
+const d = new Date();
+console.log(d.getDate()+":" + d.getMonth()+":" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + "." + d.getSeconds());
